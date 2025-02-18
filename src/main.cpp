7e71e1e0318020f915b8ec1a7e2ad869f4ae3867
MikeyMo <mamedeo@me.com>
2025-02-18 14:44:15 -0700

    Initial project setup with CMake and SDL2

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main() {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("LumonMDR",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    // Cleanup
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
